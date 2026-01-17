f474cd5ee2438bbc919a36bb764e8b859cf3a7cf
rafaelmirandadev <rmiranda.class@gmail.com>
2026-01-17 19:44:55 -0300

    meu primeiro programa em java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        String m2 = "Olá mundo";
+
+        System.out.println("Hello World");
+        System.out.println(m2);
+    }
+}
